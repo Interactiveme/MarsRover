a3566bd26706bd7026f4a99ad70f2bf886448e2a
Joseph Smith <joseph.smith@interactiveme.co.nz>
2016-02-27 18:14:15 +1300

    Added move forward functionality and refactor rotate method

--- app/rover.py
+++ app/rover.py
@@ -3,35 +3,48 @@ class Rover:
         self._x = initial_x
         self._y = initial_y
         self._heading = initial_heading
+        self.directions = ['N', 'E', 'S', 'W']
+
+        self._backward = -1
+        self._forwards = 1
+        self.forwards_movements = {
+            'N': self._increment_y,
+            'E': self._increment_x,
+            'S': self._decrement_y,
+            'W': self._decrement_x
+        }
+
+    def _get_direction_index(self):
+        index = self.directions.index(self._heading)
+        return index
+
+    def _increment_x(self):
+        self._x += 1
+
+    def _increment_y(self):
+        self._y += 1
+
+    def _decrement_x(self):
+        self._x -= 1
+
+    def _decrement_y(self):
+        self._y -= 1
 
     def rotate_left(self):
-        if self._heading == 'N':
-            self._heading = 'W'
-        elif self._heading == 'E':
-            self._heading = 'N'
-        elif self._heading == 'S':
-            self._heading = 'E'
-        elif self._heading == 'W':
-            self._heading = 'S'
+
+        index = self._get_direction_index() - 1
+        self._heading = self.directions[index]
 
     def rotate_right(self):
-        if self._heading == 'N':
-            self._heading = 'E'
-        elif self._heading == 'E':
-            self._heading = 'S'
-        elif self._heading == 'S':
-            self._heading = 'W'
-        elif self._heading == 'W':
-            self._heading = 'N'
 
-    def is_heading_west(self):
-        return self._heading == 'W'
+        index = self._get_direction_index() + 1
+        if index == len(self.directions):
+            index = 0
 
-    def is_heading_north(self):
-        return self._heading == 'N'
+        self._heading = self.directions[index]
 
-    def is_heading_south(self):
-        return self._heading == 'S'
+    def move_forward(self):
+        self.forwards_movements[self._heading]()
 
-    def is_heading_east(self):
-        return self._heading == 'E'
+    def get_current_location(self):
+        return {'x': self._x, 'y': self._y, 'heading': self._heading}
--- test_rover.py
+++ test_rover.py
@@ -3,11 +3,26 @@ from app.rover import Rover
 
 class TestRover:
 
+    def test_rover_can_move_forward(self):
+        rover = Rover(5, 5, 'N')
+        rover.move_forward()
+        current = rover.get_current_location()
+        assert current['y'] == 6
+
+        rover.rotate_left()
+        rover.move_forward()
+        current = rover.get_current_location()
+        assert current['y'] == 6
+        assert current['x'] == 4
+        assert current['heading'] == 'W'
+
     def test_rover_can_rotate_left(self):
         rover = Rover(5, 5, 'N')
-        headings = ['N', 'W', 'S', 'E']
+        headings = ['N', 'W', 'S', 'E', 'N', 'W', 'S', 'E']
+
         for i in headings:
             rover.rotate_left()
+            print rover._heading
             if i == 'N':
                 assert rover.is_heading_west() == True
 
@@ -22,7 +37,7 @@ class TestRover:
 
     def test_rover_can_rotate_right(self):
         rover = Rover(5, 5, 'N')
-        headings = ['N', 'E', 'S', 'W']
+        headings = ['N', 'E', 'S', 'W', 'N', 'E', 'S', 'W']
         for i in headings:
             rover.rotate_right()
             if i == 'N':
